5e6c1a0a58b394b40ef3ef749c14303e3ae9054d
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2024-01-27 19:58:09 +0530

    Add files via upload

---
+++ Two Sum II - Input Array Is Sorted.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int i=0,j=arr.size()-1;
+        while(i<j){
+            int x=arr[i]+arr[j];
+            if(target==x)
+                return {i+1,j+1};
+            else if(target<x)
+                j--;
+            else
+                i++;
+        }
+        return {};
+    }
+};
+
+/*
+	Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+	Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+	The tests are generated such that there is exactly one solution. You may not use the same element twice.
+	Your solution must use only constant extra space.
+	
+	Input: numbers = [2,7,11,15], target = 9
+	Output: [1,2]
+	Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+	
+	T.C :O(n)
+*/
